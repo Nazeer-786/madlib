5b9cae14402c4873a40b682213741e367fe2a2d9
Shaik Nazeer <146717256+Nazeer-786@users.noreply.github.com>
2024-06-22 16:47:15 +0530

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,40 @@
+#madlib project 
+'''In this project there given a text with some blanks, where user has to fill the blanks 
+ with suitable meaning '''
+Noun1 = input("Enter Noun1 : ") # Python
+Adjective1 = input("Enter Adjective1 : ") # readability
+Adjective2 = input("Enter Adjective2 : ") # simplicity
+Noun2 = input("Enter Noun2 : ") # Guido van Rossum
+Verb1 = input("Enter Verb1 : ") # grown
+Noun3 = input("Enter Noun3 : ") #programming
+Adjective3 = input("Enter Adjective3 : ") # excellent
+Adjective4 = input("Enter Adjective4 : ") # multiple
+Pronoun1 = input('Enter pronoun1 : ') # which
+Noun4 = input("Enter Noun4 : ") # one 
+Verb2 = input("Enter Verb2 : ") #  provides
+Preposition1 = input("Enter Preposition1 : ") # from
+Preposition2 = input("Enter Preposition2 : ") # through
+Adjective5 = input("Enter Adjective5 : ") # active
+Verb2 = input("Enter Verb2 : ")  # learning
+
+
+# A simple paragraph about the python programming language 
+
+madlib = f"{Noun1} is a versatile, high-level programming language known for its \
+{Adjective1} and {Adjective2}. Created by {Noun2}  and first released \
+in 1991, Python has {Verb1} to become one of the most popular {Noun3} languages in \
+the world. Its clear syntax and dynamic typing make it an {Adjective3} choice for both \
+beginners and experienced developers. Python supports {Adjective4} programming paradigms, \
+including procedural, object-oriented, and functional programming, {Pronoun1} allows developers \
+to choose the style that best suits their needs.{Noun4} of Python's greatest strengths is its \
+extensive standard library, which {Verb2} tools suited to many tasks, {Preposition1} web development \
+to data analysis and scientific computing. Additionally, Python has a rich ecosystem of \
+third-party packages available {Preposition2} the Python Package Index (PyPI), enabling developers \
+to easily extend the language's capabilities. Python's community is {Adjective5} and vibrant, \
+contributing to a wealth of tutorials, documentation, and forums that foster continuous {Verb2} and collaboration."
+ 
+# printing the above paragraph after filling the blanks 
+
+print(madlib)
+
+
